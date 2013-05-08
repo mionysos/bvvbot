3b7898d5bcbbd57eca8f8642f34f87e54e1d4d74
Mionysos <d.mitterneder@rediska.de>
2013-05-08 12:50:07 +0300

    Update bvvupdates.py

--- bvvupdates.py
+++ bvvupdates.py
@@ -9,9 +9,9 @@ from BeautifulSoup import BeautifulSoup	# HTML Parser
 from twitter import *					# Bibliothek auf https://github.com/sixohsix/twitter 
 
 # folgende Schluessel nach Erzeugen einer App auf Twitter erhalten ( @BVVupdates )
-CONSUMER_KEY 		= "XXX"									#consumer
+CONSUMER_KEY 		= "XXX"			#consumer
 CONSUMER_SECRET 	= "XXX"			#consumer secret
-OAUTH_TOKEN 		= "XXX"	#access token
+OAUTH_TOKEN 		= "XXX"			#access token
 OAUTH_SECRET  		= "XXX"			#access token secret
 
 # 
@@ -36,10 +36,10 @@ def hole_drucksachendict(drucksachenquelle):
 	for entry in table:
 		set={}
 		try:
-			set["titel"]			= entry.findAll('a')[0].string.encode('utf-8', 'ignore')
-			set["initiator"]		= entry.findAll('td')[3].string.encode('utf-8', 'ignore')
+			set["titel"]		= entry.findAll('a')[0].string.encode('utf-8', 'ignore')
+			set["initiator"]	= entry.findAll('td')[3].string.encode('utf-8', 'ignore')
 			set["drucksachenart"]	= entry.findAll('td')[5].string.encode('utf-8', 'ignore')
-			set["link"]				= entry.a['href'].encode('utf-8', 'ignore')
+			set["link"]		= entry.a['href'].encode('utf-8', 'ignore')
 			if not "NPD" in set["initiator"]:
 				drucksachendict[int(entry.td.form.input['value'])] = set
 		except:
@@ -48,22 +48,21 @@ def hole_drucksachendict(drucksachenquelle):
 	return drucksachendict
 
 def tweetsenden(tweettext):
-	t 				= Twitter(auth=OAuth(OAUTH_TOKEN,OAUTH_SECRET,CONSUMER_KEY,CONSUMER_SECRET))
+	t 			= Twitter(auth=OAuth(OAUTH_TOKEN,OAUTH_SECRET,CONSUMER_KEY,CONSUMER_SECRET))
 	t.statuses.update(status=tweettext) 
 
 def komponiere_tweettext(bezirksschluessel,drucksache):
 	
 	tweet_drucksachenart 	= drucksache["drucksachenart"]
-	tweet_initiator 		= drucksache["initiator"]
-	tweet_titel 			= drucksache["titel"].split("&nbsp;")[1] # noch unschöne Stelle
-	tweet_titel.replace('&ndash;', '')
-	tweet_link 				= drucksache["link"]
+	tweet_initiator 	= drucksache["initiator"]
+	tweet_titel 		= drucksache["titel"].split("&nbsp;")[1] # noch unschöne Stelle
+	tweet_link 		= drucksache["link"]
 	# Zusammensetzen des Tweets: 1. Versuch
 	tweet_bezirk = bezirke[bezirksschluessel][2]
 	tweettext = tweet_drucksachenart+" - "+tweet_initiator+" in "+tweet_bezirk+": "+tweet_titel+" http://www.berlin.de"+tweet_link
 	# Zeichenlimit von Twitter beachten und ggf Neukomposition
 	if len(tweettext)>141:
-		differenz 		= len(tweettext)-175
+		differenz 	= len(tweettext)-175
 		len_tweet_titel = len(tweet_titel)-differenz
 		tweet_titel 	= tweet_titel[:len_tweet_titel]+"..."
 	# Zusammensetzen des Tweets: 2. Versuch
@@ -80,14 +79,14 @@ print letzte_volfdnr
 for bezirksschluessel in bezirke.keys():
 	print bezirksschluessel
 	drucksachenquelle = 'http://www.berlin.de/'+bezirke[bezirksschluessel][0]+'/bvv-online/vo040.asp'+bezirke[bezirksschluessel][3]
-	drucksachendict = hole_drucksachendict(drucksachenquelle)
-	alle_volfdnr = drucksachendict.keys()
-	neue_volfndr = []
+	drucksachendict 	= hole_drucksachendict(drucksachenquelle)
+	alle_volfdnr 		= drucksachendict.keys()
+	neue_volfndr 		= []
 	for volfdnr in alle_volfdnr:
 		if int(volfdnr)>letzte_volfdnr[bezirksschluessel]:
 			neue_volfndr.append(int(volfdnr))
-			drucksache = drucksachendict[volfdnr]
-			tweettext = komponiere_tweettext(bezirksschluessel,drucksache)
+			drucksache 	= drucksachendict[volfdnr]
+			tweettext 	= komponiere_tweettext(bezirksschluessel,drucksache)
 			print tweettext
 			try:
 				tweetsenden(tweettext)
